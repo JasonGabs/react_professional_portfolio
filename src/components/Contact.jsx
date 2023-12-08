7174dfe643f919eb61579a5de376b617c4eab683
JasonGabs <jasongabs@gmail.com>
2023-12-07 18:54:14 -0800

    Added extra styling to bottom sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
             <div id="contact" className=" bg-blue-300 font-serif p-8 text-5xl text-center">
                 Contact
             </div>
-            <div className="bg-blue-300 font-serif text-3xl p-4">
+            <div className="bg-blue-200 font-serif text-3xl p-4">
                 Click on the link for my LinkedIn if you'd like to contact me:
             </div>
-            <a href="https://www.linkedin.com/in/jason-gabianelli-640b20212/" target="_blank" rel="noreferrer" className="flex truncate font-serif bg-blue-300 text-3xl p-4 min-w-full">
+            <a href="https://www.linkedin.com/in/jason-gabianelli-640b20212/" target="_blank" rel="noreferrer" className="flex truncate font-serif bg-blue-300 text-3xl text-blue-700 p-4 min-w-full underline">
                 https://www.linkedin.com/in/jason-gabianelli-640b20212/
             </a>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
     return (
         <>
-            <div className="flex p-4 font-serif bg-emerald-100 text-3xl min-w-full">
-                Click here for my github where these projects can be found:
+            <div className="flex p-4 font-serif bg-emerald-200 text-3xl min-w-full">
+                Click on the link for my github where these projects can be found:
             </div>
-            <a href="https://github.com/JasonGabs" target="_blank" rel="noreferrer" className="flex truncate p-4 font-serif bg-emerald-100 text-3xl min-w-full">
+            <a href="https://github.com/JasonGabs" target="_blank" rel="noreferrer" className="flex truncate p-4 font-serif bg-emerald-100 text-3xl text-blue-700 min-w-full underline bg-emerald-100">
                 https://github.com/JasonGabs
             </a>
-            <div className="flex-col font-serif h-40 p-4  bg-emerald-100 text-5xl text-center justify-items-center">
+            <div className="flex-col font-serif h-40 p-4  bg-emerald-100 text-5xl text-center justify-items-center bg-emerald-200">
                 Thanks for visiting!
             </div>
         </>
